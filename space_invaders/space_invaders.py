d3c8976dad65eb5f9bd2e7e0194b522f71db414e
seb olew <olewczynski@gmail.com>
2025-01-07 23:01:15 +0100

    space invaders start

---
+++ space_invaders/space_invaders.py
@@ -0,0 +1,19 @@
+import pygame, random
+
+pygame.init()
+
+
+# set display properties
+WINDOW_WIDTH = 1200
+WINDOW_HEIGHT = 700
+display_surface = pygame.display.set_mode((WINDOW_WIDTH, WINDOW_HEIGHT))
+pygame.display.set_caption('Space Invaders')
+
+
+# main game loop
+running = True
+while running:
+    pass
+
+
+pygame.quit()

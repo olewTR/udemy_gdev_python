57f746e57b01b8638f014f58eef925dada74620f
olewTR <olew@poczta.onet.pl>
2025-01-24 16:32:12 +0000

    tile_map continued

--- platformer_pygame/2_creating_level.py
+++ platformer_pygame/2_creating_level.py
@@ -15,7 +15,7 @@ FPS = 60
 # define classes
 class Tile(pygame.sprite.Sprite):
     """ a class to read and create individual tiles """
-    def __init__(self, x, y, image_int,main_group, sub_group=""):
+    def __init__(self, x, y, image_int, main_group, sub_group=""):
         super().__init__()
         # load images and put them to proper groups
         if image_int == 1:
@@ -25,10 +25,14 @@ class Tile(pygame.sprite.Sprite):
             sub_group.add(self)
         elif image_int == 3:
             self.image = pygame.image.load('./assets/water.png')
+            sub_group.add(self)
 
         # all tiles should go to the main group
         main_group.add(self)
 
+        # get the rect of the image and position it on grid
+        self.rect = self.image.get_rect()
+        self.rect.topleft = (x,y)
 
 # create sprite groups
 main_tile_group = pygame.sprite.Group()
@@ -64,12 +68,28 @@ tile_map = [
     [1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 1, 3, 3, 3, 3, 3, 3, 3, 3, 1, 1, 1, 1, 1, 1 ]
 ]
 
+# create individual object from the tile map
+
+for i in range (len(tile_map)):
+    for j in range (len(tile_map[0])):
+        if tile_map[i][j] == 1:
+            Tile(j * 32, i*32, 1, main_tile_group)
+        elif tile_map[i][j] == 2:
+            Tile(j * 32, i*32, 2, main_tile_group, gras_tile_group)
+        elif tile_map[i][j] == 3:
+            Tile(j * 32, i*32, 3, main_tile_group, water_tile_group)
+
 # main game loop
 is_running = True
 while is_running:
     for event in pygame.event.get():
         if event.type == pygame.QUIT:
             is_running = False
+
+    #fill the display 
+    display_surface.fill((10, 75, 75))
+
+    main_tile_group.draw(display_surface)
 
     pygame.display.update()
     clock.tick(FPS)
